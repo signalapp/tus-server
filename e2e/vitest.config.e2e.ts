5f13e290c8e642d304562984e67a23acc158c338
Ravi Khadiwala <ravi@signal.org>
2025-07-10 15:32:33 -0500

    Remove miniflare test environment

--- e2e/vitest.config.e2e.ts
+++ e2e/vitest.config.e2e.ts
@@ -1,18 +1,8 @@
 // Copyright 2024 Signal Messenger, LLC
 // SPDX-License-Identifier: AGPL-3.0-only
 
+// The e2e tests use a standard node test runner instead of vitest-pool-workers, since they launch an entire listening
+// worker
 import {defineConfig} from 'vitest/config';
 
-export default defineConfig({
-    test: {
-        environment: 'miniflare',
-        environmentOptions: {
-            modules: true,
-            scriptPath: 'dist/index.js',
-            durableObjects: {
-                ATTACHMENT_UPLOAD_HANDLER: 'AttachmentUploadHandler',
-                BACKUP_UPLOAD_HANDLER: 'BackupUploadHandler',
-            },
-        },
-    },
-});
+export default defineConfig({});
